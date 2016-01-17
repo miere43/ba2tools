6b0ed4540aa0b1d1a852be5d769e808584919ddf
miere43 <x.miere@gmail.com>
2016-01-17 17:53:38 +0300

    Documentation updates.

--- Source/Ba2ToolsTests/Ba2ArchiveLoaderTests.cs
+++ Source/Ba2ToolsTests/Ba2ArchiveLoaderTests.cs
@@ -8,14 +8,30 @@ using System.IO;
 
 namespace Ba2Tools.Tests
 {
+    /// <summary>
+    /// Data shared between tests.
+    /// </summary>
     public static class SharedData
     {
+        /// <summary>
+        /// Folder with test data.
+        /// </summary>
         public static string DataFolder = "../../Data/";
 
+        /// <summary>
+        /// Folder to store temp data.
+        /// </summary>
         public static string TempFolder = "../../Temp/";
 
+        /// <summary>
+        /// BA2 archive magic.
+        /// </summary>
         public static byte[] ArchiveMagic = new byte[] { 0x42, 0x54, 0x44, 0x58 };
 
+        /// <summary>
+        /// Creates random temp folder in <c>TempFolder</c> and returns its path.
+        /// </summary>
+        /// <returns>Path to temp folder.</returns>
         public static string CreateTempDirectory()
         {
             string path = Path.Combine(TempFolder, Path.GetRandomFileName());
@@ -23,6 +39,9 @@ namespace Ba2Tools.Tests
             return path;
         }
 
+        /// <summary>
+        /// Cleans temporary files and directories recursively.
+        /// </summary>
         public static void CleanupTemp()
         {
             foreach (var dir in Directory.EnumerateDirectories(TempFolder))
