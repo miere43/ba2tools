3f642e95d8127fcc912dda99d61f8549325c3d1e
miere43 <x.miere@gmail.com>
2016-01-17 13:37:09 +0300

    Added forgotten file

---
+++ Source/Ba2Tools/BA2TextureArchive.cs
@@ -0,0 +1,320 @@
+﻿using Ba2Tools.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ba2Tools
+{
+    /// <summary>
+    /// Represents texture BA2 archive type.
+    /// </summary>
+    /// <remarks>Should not be used, work in progress.</remarks>
+    public sealed class BA2TextureArchive : BA2Archive
+    {
+        private BA2TextureFileEntry[] fileEntries = null;
+
+        /// <summary>
+        /// Preload file entries. Should be called only once.
+        /// </summary>
+        /// <param name="reader"></param>
+        internal override void PreloadData(BinaryReader reader = null)
+        {
+            reader.BaseStream.Seek(BA2Loader.HeaderSize, SeekOrigin.Begin);
+            fileEntries = new BA2TextureFileEntry[TotalFiles];
+
+            for (int i = 0; i < TotalFiles; i++)
+            {
+                BA2TextureFileEntry entry = new BA2TextureFileEntry()
+                {
+                    Unknown0 = reader.ReadUInt32(),
+                    Extension = Encoding.ASCII.GetChars(reader.ReadBytes(4)),
+                    Unknown1 = reader.ReadUInt32(),
+                    Unknown2 = reader.ReadByte(),
+                    NumberOfChunks = reader.ReadByte(),
+                    ChunkHeaderSize = reader.ReadUInt16(),
+                    TextureHeight = reader.ReadUInt16(),
+                    TextureWidth = reader.ReadUInt16(),
+                    NumberOfMipmaps = reader.ReadByte(),
+                    Format = reader.ReadByte(),
+                    Unknown3 = reader.ReadUInt16()
+                };
+
+                ReadChunksForEntry(reader, ref entry);
+
+                // 3131961357 = 0xBAADF00D as uint little-endian (0x0DF0ADBA)
+                // System.Diagnostics.Debug.Assert(entry.Unknown3 == 3131961357);
+
+                fileEntries[i] = entry;
+            }
+        }
+
+        private void ReadChunksForEntry(BinaryReader reader, ref BA2TextureFileEntry entry)
+        {
+            var chunks = new TextureChunk[entry.NumberOfChunks];
+
+            for (int i = 0; i < entry.NumberOfChunks; i++)
+            {
+                TextureChunk chunk = new TextureChunk()
+                {
+                    Offset = reader.ReadUInt64(),
+                    PackedLength = reader.ReadUInt32(),
+                    UnpackedLength = reader.ReadUInt32(),
+                    StartMipmap = reader.ReadUInt16(),
+                    EndMipmap = reader.ReadUInt16(),
+                    Unknown = reader.ReadUInt32()
+                };
+
+                chunks[i] = chunk;
+            }
+
+            entry.Chunks = chunks;
+        }
+
+        private BA2TextureFileEntry? GetEntryFromName(ref string fileName)
+        {
+            if (_fileListCache == null)
+                ListFiles();
+
+            for (int i = 0; i < _fileListCache.Length; i++)
+            {
+                string name = _fileListCache[i];
+
+                if (name.Equals(fileName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return fileEntries[i];
+                }
+            }
+
+            return null;
+        }
+
+        public override void ExtractAll(string destination, bool overwriteFiles = false)
+        {
+            if (_fileListCache == null)
+                ListFiles();
+
+            foreach (var f in _fileListCache)
+            {
+                Extract(f, destination, overwriteFiles);
+            }
+        }
+
+        private BA2TextureFileEntry? GetEntryFromName(string fileName)
+        {
+            if (_fileListCache == null)
+                ListFiles();
+
+            for (int i = 0; i < _fileListCache.Length; i++)
+            {
+                string name = _fileListCache[i];
+
+                if (name.Equals(fileName, StringComparison.OrdinalIgnoreCase))
+                {
+                    return fileEntries[i];
+                }
+            }
+
+            return null;
+        }
+
+        public override bool ExtractToStream(string fileName, Stream stream)
+        {
+            if (fileName == null)
+                throw new ArgumentNullException(nameof(fileName));
+            if (stream == null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (!stream.CanWrite)
+                return false;
+
+            var entry = GetEntryFromName(fileName);
+            if (!entry.HasValue)
+                return false;
+
+            using (var archive = File.OpenRead(FilePath))
+            {
+                var e = entry.Value;
+                ExtractToStream(ref e, archive, stream);
+                return true;
+            }
+        }
+
+        public override void ExtractFiles(string[] fileNames, string destination, bool overwriteFiles = false)
+        {
+            if (fileNames == null)
+                throw new ArgumentNullException("fileNames is null");
+            if (string.IsNullOrWhiteSpace(destination))
+                throw new ArgumentException("destination is invalid");
+            if (fileNames.Length > TotalFiles)
+                throw new BA2ExtractionException("fileNames length is more than total files in archive");
+
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                Extract(fileNames[i], destination, false);
+            }
+        }
+
+        public override void Extract(string fileName, string destination, bool overwriteFile = false)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentException("fileName is invalid");
+            if (string.IsNullOrWhiteSpace(destination))
+                throw new ArgumentException("destination is invalid");
+
+            if (!Directory.Exists(destination))
+                Directory.CreateDirectory(destination);
+
+            if (_fileListCache == null)
+                ListFiles(true);
+
+            BA2TextureFileEntry? fileEntryNullable = GetEntryFromName(ref fileName);
+            if (!fileEntryNullable.HasValue)
+                throw new BA2ExtractionException("Cannot find file name \"" + fileName + "\" in archive");
+
+            var fileEntry = fileEntryNullable.Value;
+
+            string extension = new string(fileEntry.Extension).Trim('\0');
+            string finalPath = Path.Combine(destination, fileName);
+
+            string finalDest = Path.GetDirectoryName(finalPath);
+            if (!Directory.Exists(finalDest))
+                Directory.CreateDirectory(finalDest);
+
+            if (File.Exists(finalPath) && overwriteFile == false)
+                throw new BA2ExtractionException("Overwrite is not permitted.");
+
+            using (var archiveStream = File.OpenRead(FilePath))
+            {
+                using (var fileStream = File.OpenWrite(finalPath))
+                {
+                    ExtractToStream(ref fileEntry, archiveStream, fileStream);
+                }
+            }
+        }
+
+        private void ExtractToStream(ref BA2TextureFileEntry entry, Stream sourceStream, Stream destStream)
+        {
+            using (BinaryWriter writer = new BinaryWriter(destStream, Encoding.ASCII))
+            {
+                DdsHeader header = new DdsHeader();
+                FillDdsHeader(ref header, ref entry);
+
+                writer.Write(Dds.DDS_MAGIC);
+                writer.Write(header.dwSize);
+                writer.Write(header.dwHeaderFlags);
+                writer.Write(header.dwHeight);
+                writer.Write(header.dwWidth);
+                writer.Write(header.dwPitchOrLinearSize);
+                writer.Write(header.dwDepth);
+                writer.Write(header.dwMipMapCount);
+                for (int i = 0; i < 11; i++)
+                {
+                    writer.Write((uint)0);
+                }
+                writer.Write(header.ddspf.dwSize);
+                writer.Write(header.ddspf.dwFlags);
+                writer.Write(header.ddspf.dwFourCC);
+                writer.Write(header.ddspf.dwRGBBitCount);
+                writer.Write(header.ddspf.dwRBitMask);
+                writer.Write(header.ddspf.dwGBitMask);
+                writer.Write(header.ddspf.dwBBitMask);
+                writer.Write(header.ddspf.dwABitMask);
+                writer.Write(header.dwSurfaceFlags);
+                writer.Write(header.dwCubemapFlags);
+                for (int i = 0; i < 3; i++)
+                {
+                    writer.Write((uint)0);
+                }
+
+                for (uint i = 0; i < entry.NumberOfChunks; i++)
+                {
+                    var chunk = entry.Chunks[i];
+
+                    if (chunk.PackedLength == 0)
+                    {
+                        Debug.WriteLine("entry " + entry.Unknown0 + " chunk " + i + " packed length = 0");
+                    }
+
+                    sourceStream.Seek((long)chunk.Offset + 2, SeekOrigin.Begin);
+
+                    byte[] destBuffer = new byte[chunk.UnpackedLength];
+                    using (var uncompressStream = new DeflateStream(sourceStream, CompressionMode.Decompress, leaveOpen: true))
+                    {
+                        var bytesReaden = uncompressStream.Read(destBuffer, 0, (int)chunk.UnpackedLength);
+                        // Debug.Assert(bytesReaden == chunk.UnpackedLength);
+                    }
+
+                    writer.Write(destBuffer, 0, (int)chunk.UnpackedLength);
+                } 
+                
+                writer.Seek(0, SeekOrigin.Begin);
+            }
+        }
+
+        private void FillDdsHeader(ref DdsHeader header, ref BA2TextureFileEntry entry)
+        {
+            DxgiFormat format = (DxgiFormat)entry.Format;
+
+            header.dwSize = 124; // sizeof(DDS_HEADER)
+            header.dwHeaderFlags = Dds.DDS_HEADER_FLAGS_TEXTURE | 
+                Dds.DDS_HEADER_FLAGS_LINEARSIZE | Dds.DDS_HEADER_FLAGS_MIPMAP;
+            header.dwHeight = (uint)entry.TextureHeight;
+            header.dwWidth = (uint)entry.TextureWidth;
+            header.dwMipMapCount = (uint)entry.NumberOfMipmaps;
+            header.ddspf.dwSize = 32; // sizeof(DDS_PIXELFORMAT);
+            header.dwSurfaceFlags = Dds.DDS_SURFACE_FLAGS_TEXTURE | Dds.DDS_SURFACE_FLAGS_MIPMAP;
+
+            switch (format)
+            {
+                case DxgiFormat.BC1_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_FOURCC;
+                    header.ddspf.dwFourCC = (uint)Dds.MakeFourCC('D', 'X', 'T', '1');
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight / 2u;
+                    break;
+                case DxgiFormat.BC2_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_FOURCC;
+                    header.ddspf.dwFourCC = (uint)Dds.MakeFourCC('D', 'X', 'T', '3');
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight;
+                    break;
+                case DxgiFormat.BC3_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_FOURCC;
+                    header.ddspf.dwFourCC = (uint)Dds.MakeFourCC('D', 'X', 'T', '5');
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight;
+                    break;
+                case DxgiFormat.BC5_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_FOURCC;
+                    // ATI2
+                    header.ddspf.dwFourCC = (uint)Dds.MakeFourCC('D', 'X', 'T', '5');
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight;
+                    break;
+                case DxgiFormat.BC7_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_FOURCC;
+                    header.ddspf.dwFourCC = (uint)Dds.MakeFourCC('B', 'C', '7', '\0');
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight;
+                    break;
+                case DxgiFormat.B8G8R8A8_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_RGBA;
+                    header.ddspf.dwRGBBitCount = 32;
+                    header.ddspf.dwRBitMask = 0x00FF0000;
+                    header.ddspf.dwGBitMask = 0x0000FF00;
+                    header.ddspf.dwBBitMask = 0x000000FF;
+                    header.ddspf.dwABitMask = 0xFF000000;
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight * 4u;
+                    break;
+                case DxgiFormat.R8_UNORM:
+                    header.ddspf.dwFlags = Dds.DDS_RGB;
+                    header.ddspf.dwRGBBitCount = 8;
+                    header.ddspf.dwRBitMask = 0xFF;
+                    header.dwPitchOrLinearSize = (uint)entry.TextureWidth * (uint)entry.TextureHeight;
+                    break;
+                default:
+                    throw new NotImplementedException("DDS format " + format.ToString() + " is not supported.");
+            }
+        }
+    }
+}
