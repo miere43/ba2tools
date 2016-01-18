8ba21fb071855f6e4f3b93f2500c4798d8f5f3a5
miere43 <x.miere@gmail.com>
2016-01-18 15:00:48 +0300

    Added more tests.

--- Source/Ba2ToolsTests/Data/README.md
+++ Source/Ba2ToolsTests/Data/README.md
@@ -2,5 +2,7 @@
 
 **Don't forget to update tests when updating test archives** 
 
-* GeneralHeaderOnly.ba2 — contains only header, 0 files, no name table
-* GeneralOneFile.ba2 — contains header, 1 text file (test.txt), 1 name table entry (test.txt)
+* GeneralHeaderOnly.ba2 — contains only header, 0 files, no name table.
+* GeneralHeaderOnlyInvalidVersion.ba2 — same as previous, but contains version 153 instead of valid version 1.
+* GeneralOneFile.ba2 — contains header, 1 text file (test.txt), 1 name table entry (test.txt).
+* InvalidArchive.txt — plain text file, not archive at all.
---
+++ Source/Ba2ToolsTests/SharedData.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace Ba2ToolsTests
+{
+    /// <summary>
+    /// Data shared between tests.
+    /// </summary>
+    public static class SharedData
+    {
+        /// <summary>
+        /// Folder with test data.
+        /// </summary>
+        public static string DataFolder = "../../Data/";
+
+        /// <summary>
+        /// Folder to store temp data.
+        /// </summary>
+        public static string TempFolder = "../../Temp/";
+
+        /// <summary>
+        /// BA2 archive magic.
+        /// </summary>
+        public static byte[] ArchiveMagic = new byte[] { 0x42, 0x54, 0x44, 0x58 };
+
+        /// <summary>
+        /// Creates random temp folder in <c>TempFolder</c> and returns its path.
+        /// </summary>
+        /// <returns>Path to temp folder.</returns>
+        public static string CreateTempDirectory()
+        {
+            string path = Path.Combine(TempFolder, Path.GetRandomFileName());
+            Directory.CreateDirectory(path);
+            return path;
+        }
+
+        /// <summary>
+        /// Cleans temporary files and directories recursively.
+        /// </summary>
+        public static void CleanupTemp()
+        {
+            foreach (var dir in Directory.EnumerateDirectories(TempFolder))
+            {
+                Directory.Delete(dir, true);
+            }
+        }
+
+        public static string GetDataPath(string item)
+        {
+            return Path.Combine(DataFolder, item);
+        }
+    }
+}
