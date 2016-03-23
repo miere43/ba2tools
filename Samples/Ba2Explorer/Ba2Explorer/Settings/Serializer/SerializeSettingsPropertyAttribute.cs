4216a9d3bcc8796df25960e1884fbd81ea9c5ec6
miere43 <x.miere@gmail.com>
2016-03-23 13:23:03 +0300

    Removed WIP sample.

--- Samples/Ba2Explorer/Ba2Explorer/Settings/Serializer/SerializeSettingsPropertyAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Ba2Explorer.Settings.Serializer
-{
-    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = true)]
-    sealed class SerializeSettingsPropertyAttribute : Attribute
-    {
-    }
-}
